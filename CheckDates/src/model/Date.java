ee182133d6b0d1c3d699e95d86d7843b2bf55949
Willian Duarte <willianm.cduarte@gmail.com>
2019-05-17 17:50:58 -0300

    created getNameOfTheMonth()

--- CheckDates/src/model/Date.java
+++ CheckDates/src/model/Date.java
@@ -64,6 +64,41 @@ public class Date {
     public void setMonth(byte month) {
         this.month = month;
     }
+    
+    /**
+     * Use the month attribute to return the month name
+     * @return The month's name
+     */
+    public String getNameOfTheMonth() {
+        switch (this.month) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                return "Invalid";
+        }
+    }
 
     @Override
     public String toString() {
